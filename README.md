6004353e597410dd52843a85af86ec476c9188aa
darwaishx <kashifimran@outlook.com>
2019-05-27 12:24:58 -0700

    IC

--- README.md
+++ README.md
@@ -1,6 +1,192 @@
-## AWS Deeplens Worker Saftey Project
+# Worker Safety with AWS DeepLens and Amazon Rekognition
 
 Build worker safety app using AWS DeepLens and Amazon Rekognition.
+
+## Learning Objectives of This lab
+In this lab your will the following:
+- Create and deploy object detection project to DeepLens.
+- Modify the DeepLens object detection inference lambda function to detect persons and upload frame to S3.
+- Create lambda function to identify persons who are not wearing safety hats.
+- Analyze results using IoT and CloudWatch and Web Dashboard.
+
+## Architecture
+
+![](arch.png)
+
+## Modules
+
+### Setup IAM Role for Cloud Lambda
+
+1. Go to IAM in AWS Console at https://console.aws.amazon.com/iam
+2. Click on Roles
+3. Click create role
+4. Under AWS service, select Lambda and click Next: Permissions
+5. Under Attach permission policies
+    1. search S3 and select AmazonS3FullAccess
+    2. search Rekognition and select checkbox next to AmazonRekognitionReadOnlyAccess
+    3. search cloudwatch and select checkbox next to CloudWatchLogsFullAccess and CloudWatchFullAccess
+    4. search iot and select AWSIotDataAccess
+    5. search lambda and select checkbox next to AWSLambdaFullAccess
+6. Click Next: Tags and Next: Review
+7. Name is “RecognizeObjectLambdaRole”
+8. Click Create role
+
+
+### Setup IAM Role for DeepLens Lambda
+
+1. Click create role
+2. Under AWS service, select Lambda and click Next: Permissions
+3. Under Attach permission policies
+    1. search S3 and select AmazonS3FullAccess
+    2. search lambda and select checkbox next to AWSLambdaFullAccess
+4. Click Next: Tags and Next: Review
+5. Name is “DeepLensInferenceLambdaRole”
+6. Click Create role
+
+
+### Create S3 bucket
+
+1. Go to Amazon S3 in AWS Console at https://s3.console.aws.amazon.com/s3/
+2. Click on Create bucket.
+3. Under Name and region:
+
+* Bucket name: Enter a bucket name- your name-worker-safety (example: kashif-worker-safety)
+* Choose US East (N. Virginia)
+* Click Next
+
+1. Leave default values for Configure Options screen and click Next
+2.  Under Set permissions, uncheck all four checkboxes. NOTE: This step would allow us to make objects in your S3 bucket public. We are doing this to reduce few steps in the module, but you should not do that for production workloads. Instead it is recommended to use S3 Pre-Signed URLs to give time limited access to objects in S3.
+3. Click Next, and click Create bucket.
+
+### Create Cloud Lambda
+
+1. Go to Lambda in AWS Console at https://console.aws.amazon.com/lambda/
+2. Click on Create function.
+3. Under Create function, Author from scratch should be selected as default.
+4. Under Author from scratch:
+
+* Name: worker-safety-cloud
+* Runtime: Python 3.7
+* Role: Choose and existing role
+* Existing role: RecognizeObjectLambdaRole
+* Click Create function
+
+1. Under Environment variables, add a variable:
+
+* Key: iot_topic
+* Value: worker-safety-demo-cloud
+
+1. Download [lambda.zip](./code/lambda.zip).
+2. Under Function code:
+
+* Code entry type: Upload a zip file
+* Under Function package, click Upload and select the zip file you downloaded in earlier step.
+* Click Save.
+
+1. Under Add triggers, select S3.
+2. Under Configure triggers:
+
+* Bucket: Select the S3 bucket you just created in earlier step.
+* Event type: Leave default Object Created (All)
+* Leave defaults for Prefix and Suffix and make sure Enable trigger checkbox is checked.
+* Click Add.
+* Click Save on the top right to save changed to Lambda function.
+
+### Create DeepLens Inference Lambda Function
+
+1. Go to Lambda in AWS Console at https://console.aws.amazon.com/lambda/.
+2. Click on Create function.
+3. Under Create function, select Blueprints.
+4. Under Blueprints, type greengrass and hit enter to filter blueprint templates.
+5. Select greengrass-hello-world and click Configure.
+6. Under Basic information:
+
+* Name: name-worker-safety-deeplens (example: kashif-worker-safety-deeplens)
+* Role: Choose and existing role
+* Existing role: DeepLensInferenceLambdaRole
+* Click Create function.
+
+1. Copy the code from [deeplens-lambda.py](./code/deeplens-lambda.py) and paste under Function code for the lambda function. You can find the python file in your resources section.
+2. Go to line 34 and modify line below with the name of your S3 bucket created in the earlier step.
+
+* bucket_name = "REPLACE-WITH-NAME-OF-YOUR-S3-BUCKET"
+
+1. Click Save.
+2. Click on Actions, and then "Publish new version".
+3. For Version description enter: Detect person and push frame to S3 bucket. and click Publish.
+
+### Create DeepLens Project
+
+1. Using your browser, open the AWS DeepLens console at https://console.aws.amazon.com/deeplens/.
+2. Choose Projects, then choose Create new project.
+3. On the Choose project type screen
+
+* Choose Create a new blank project, and click Next.
+
+1. On the Specify project details screen
+
+    * Under Project information section:
+        * Project name: your-user-name-worker-safety (example: kashif-worker-safety)
+    * Under Project content:
+        * Click on Add model, click on radio button for deeplens-object-detection and click Add model.
+        * Click on Add function, click on radio button for your lambda function (example: kashif-worker-safety-deeplens) lambda function and click Add function.
+* Click Create. This returns you to the Projects screen.
+
+### Deploy DeepLens Project
+
+1. From DeepLens console, On the Projects screen, choose the radio button to the left of your project name, then choose Deploy to device.
+2. On the Target device screen, from the list of AWS DeepLens devices, choose the radio button to the left of the device where you want to deploy this project.
+3. Choose Review. This will take you to the Review and deploy screen.
+    If a project is already deployed to the device, you will see a warning message "There is an existing project on this device. Do you want to replace it? If you Deploy, AWS DeepLens will remove the current project before deploying the new project."
+4. On the Review and deploy screen, review your project and click Deploy to deploy the project. This will take you to to device screen, which shows the progress of your project deployment.
+
+### View Output in IoT
+
+1. Go to IoT Console at https://console.aws.amazon.com/iot/home
+2. Under Subscription topic enter topic name you entered as environment variable for Lambda in earlier step (example: worker-safety-demo-cloud) and click Subscribe to topic.
+3. You should now see JSON message with a list of people detected and whether they are wearing safety hats or not.
+
+### View Output in CloudWatch
+
+* Go to CloudWatch Console at https://console.aws.amazon.com/cloudwatch
+* Create a dashboard called “worker-safety-dashboard-your-name”
+* Choose Line in the widget
+* Under Custom Namespaces, select “string”, “Metrics with no dimensions”, and then select PersonsWithSafetyHat and PersonsWithoutSafetyHat.
+* Next, set “Auto-refresh” to the smallest interval possible (1h), and change the “Period” to whatever works best for you (1 second or 5 seconds)
+
+### View Output in Web Dashboard
+
+1. Go to AWS Cognito console at https://console.aws.amazon.com/cognito
+2. Click on Manage Identity Pools
+3. Click on Create New Identity Pool
+4. Enter “awsworkersafety” for Identity pool name
+5. Select Enable access to unauthenticated identities
+6. We are using using Unauthenticated identity option to keep things simple in the demo. For real world application where you only want authorized users to access the app you should configure Authentication providers.
+7. Click Create Pool
+8. Expand View Details
+9. Under: Your unauthenticated identities would like access to Cognito, expand View Policy Document and click Edit.
+10. Click Ok for Edit Policy prompt.
+11. Copy JSON from [cognitopolicy.json](./code/cognitopolicy.json) and paste in the text box.
+12. Click Allow
+13. Make note of the Identity Pool as you will need it in following steps.
+14. Got to IoT in AWS Console at: https://console.aws.amazon.com/iot
+15. Click on settings and make note of Endpoint, you will need this the following step.
+16. Download [webdashboard.zip](./code/webdashboard.zip) and unzip on your local drive.
+17. Edit aws-configuration.js and update poolId with Cognito Identity Pool Id and host with IoT EndPoint you got in earlier steps.
+18. From terminal go to the root of the unzipped folder and run “npm install”
+19. Next, run “./node_modules/.bin/webpack —config webpack.config.js”
+20. This will create the build we can easily deploy.
+21. Go to S3 bucket, and create a folder web
+22. From web folder in S3 bucket click upload and select bundle.js, index.html and style.css.
+23. From Set permission, Choose Grant public read access to the objects. and click Next
+24. Leave default settings for following screens and click upload.
+25. Click on index.html and click on the link to open the web page in browser.
+26. In the address URL append ?iottopic=NAME-OF-YOUR-IOT-TOPIC. This is the same value you added to Lambda environment variable and hit Enter.
+27. You should now see images coming from DeepLens with a green or red box around the person.
+
+
+## Clean Up
+Delete Lambda functions, S3 bucket and IAM roles.
 
 ## License Summary
 
---
+++ code/deeplens-lambda.py
@@ -0,0 +1,230 @@
+#*****************************************************
+#                                                    *
+# Copyright 2018 Amazon.com, Inc. or its affiliates. *
+# All Rights Reserved.                               *
+#                                                    *
+#*****************************************************
+""" A sample lambda for object detection"""
+from threading import Thread, Event
+import os
+import json
+import numpy as np
+import awscam
+import cv2
+import greengrasssdk
+import time
+import base64
+import urllib
+import zipfile
+import sys
+import datetime
+
+#boto3 is not installed on device by default.
+
+boto_dir = '/tmp/boto_dir'
+if not os.path.exists(boto_dir):
+    os.mkdir(boto_dir)
+urllib.urlretrieve("https://s3.amazonaws.com/dear-demo/boto_3_dist.zip", "/tmp/boto_3_dist.zip")
+with zipfile.ZipFile("/tmp/boto_3_dist.zip", "r") as zip_ref:
+    zip_ref.extractall(boto_dir)
+sys.path.append(boto_dir)
+
+import boto3
+
+bucket_name = "REPLACE-WITH-NAME-OF-YOUR-S3-BUCKET"
+
+# Create an IoT client for sending to messages to the cloud.
+client = greengrasssdk.client('iot-data')
+iot_topic = '$aws/things/{}/infer'.format(os.environ['AWS_IOT_THING_NAME'])
+
+class LocalDisplay(Thread):
+    """ Class for facilitating the local display of inference results
+        (as images). The class is designed to run on its own thread. In
+        particular the class dumps the inference results into a FIFO
+        located in the tmp directory (which lambda has access to). The
+        results can be rendered using mplayer by typing:
+        mplayer -demuxer lavf -lavfdopts format=mjpeg:probesize=32 /tmp/results.mjpeg
+    """
+    def __init__(self, resolution):
+        """ resolution - Desired resolution of the project stream """
+        # Initialize the base class, so that the object can run on its own
+        # thread.
+        super(LocalDisplay, self).__init__()
+        # List of valid resolutions
+        RESOLUTION = {'1080p' : (1920, 1080), '720p' : (1280, 720), '480p' : (858, 480)}
+        if resolution not in RESOLUTION:
+            raise Exception("Invalid resolution")
+        self.resolution = RESOLUTION[resolution]
+        # Initialize the default image to be a white canvas. Clients
+        # will update the image when ready.
+        self.frame = cv2.imencode('.jpg', 255*np.ones([640, 480, 3]))[1]
+        self.stop_request = Event()
+
+    def run(self):
+        """ Overridden method that continually dumps images to the desired
+            FIFO file.
+        """
+        # Path to the FIFO file. The lambda only has permissions to the tmp
+        # directory. Pointing to a FIFO file in another directory
+        # will cause the lambda to crash.
+        result_path = '/tmp/results.mjpeg'
+        # Create the FIFO file if it doesn't exist.
+        if not os.path.exists(result_path):
+            os.mkfifo(result_path)
+        # This call will block until a consumer is available
+        with open(result_path, 'w') as fifo_file:
+            while not self.stop_request.isSet():
+                try:
+                    # Write the data to the FIFO file. This call will block
+                    # meaning the code will come to a halt here until a consumer
+                    # is available.
+                    fifo_file.write(self.frame.tobytes())
+                except IOError:
+                    continue
+
+    def set_frame_data(self, frame):
+        """ Method updates the image data. This currently encodes the
+            numpy array to jpg but can be modified to support other encodings.
+            frame - Numpy array containing the image data of the next frame
+                    in the project stream.
+        """
+        ret, jpeg = cv2.imencode('.jpg', cv2.resize(frame, self.resolution))
+        if not ret:
+            raise Exception('Failed to set frame data')
+        self.frame = jpeg
+
+    def join(self):
+        self.stop_request.set()
+
+def push_to_s3(img):
+    try:
+        index = 0
+
+        timestamp = int(time.time())
+        now = datetime.datetime.now()
+        key = "persons/{}_{}/{}_{}/{}_{}.jpg".format(now.month, now.day,
+                                                   now.hour, now.minute,
+                                                   timestamp, index)
+
+        s3 = boto3.client('s3')
+
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+        _, jpg_data = cv2.imencode('.jpg', img, encode_param)
+        response = s3.put_object(ACL='public-read',
+                                 Body=jpg_data.tostring(),
+                                 Bucket=bucket_name,
+                                 Key=key)
+
+        client.publish(topic=iot_topic, payload="Response: {}".format(response))
+        client.publish(topic=iot_topic, payload="Frame pushed to S3")
+    except Exception as e:
+        msg = "Pushing to S3 failed: " + str(e)
+        client.publish(topic=iot_topic, payload=msg)
+
+
+def greengrass_infinite_infer_run():
+    """ Entry point of the lambda function"""
+    try:
+        # This object detection model is implemented as single shot detector (ssd), since
+        # the number of labels is small we create a dictionary that will help us convert
+        # the machine labels to human readable labels.
+        model_type = 'ssd'
+        output_map = {1: 'aeroplane', 2: 'bicycle', 3: 'bird', 4: 'boat', 5: 'bottle', 6: 'bus',
+                      7 : 'car', 8 : 'cat', 9 : 'chair', 10 : 'cow', 11 : 'dinning table',
+                      12 : 'dog', 13 : 'horse', 14 : 'motorbike', 15 : 'person',
+                      16 : 'pottedplant', 17 : 'sheep', 18 : 'sofa', 19 : 'train',
+                      20 : 'tvmonitor'}
+        # Create an IoT client for sending to messages to the cloud.
+        ###client = greengrasssdk.client('iot-data')
+        ###iot_topic = '$aws/things/{}/infer'.format(os.environ['AWS_IOT_THING_NAME'])
+        # Create a local display instance that will dump the image bytes to a FIFO
+        # file that the image can be rendered locally.
+        local_display = LocalDisplay('480p')
+        local_display.start()
+        # The sample projects come with optimized artifacts, hence only the artifact
+        # path is required.
+        model_path = '/opt/awscam/artifacts/mxnet_deploy_ssd_resnet50_300_FP16_FUSED.xml'
+        # Load the model onto the GPU.
+        client.publish(topic=iot_topic, payload='Loading object detection model')
+        model = awscam.Model(model_path, {'GPU': 1})
+        client.publish(topic=iot_topic, payload='Object detection model loaded')
+        # Set the threshold for detection
+        detection_threshold = 0.25
+        # The height and width of the training set images
+        input_height = 300
+        input_width = 300
+        # Do inference until the lambda is killed.
+        while True:
+            detectedPerson = False
+            # Get a frame from the video stream
+            ret, frame = awscam.getLastFrame()
+            if not ret:
+                raise Exception('Failed to get frame from the stream')
+            # Resize frame to the same size as the training set.
+            frame_resize = cv2.resize(frame, (input_height, input_width))
+            # Run the images through the inference engine and parse the results using
+            # the parser API, note it is possible to get the output of doInference
+            # and do the parsing manually, but since it is a ssd model,
+            # a simple API is provided.
+            parsed_inference_results = model.parseResult(model_type,
+                                                         model.doInference(frame_resize))
+            # Compute the scale in order to draw bounding boxes on the full resolution
+            # image.
+            yscale = float(frame.shape[0]/input_height)
+            xscale = float(frame.shape[1]/input_width)
+            # Dictionary to be filled with labels and probabilities for MQTT
+            cloud_output = {}
+            # Get the detected objects and probabilities
+            detectedPerson = False
+            for obj in parsed_inference_results[model_type]:
+                if obj['prob'] > detection_threshold:
+                    if(output_map[obj['label']] == 'person'):
+                        detectedPerson = True
+                        break
+
+            if(detectedPerson):
+                rfr = cv2.resize(frame, (672, 380))
+                push_to_s3(rfr)
+                #fr2 = cv2.resize(frame, (1344, 760))
+                #_, jpg_data = cv2.imencode('.jpg', fr2)
+                #push_to_s3(jpg_data)
+
+            for obj in parsed_inference_results[model_type]:
+                if obj['prob'] > detection_threshold:
+
+                    if(output_map[obj['label']] == 'person'):
+                        detectedPerson = True
+
+                    # Add bounding boxes to full resolution frame
+                    xmin = int(xscale * obj['xmin']) \
+                           + int((obj['xmin'] - input_width/2) + input_width/2)
+                    ymin = int(yscale * obj['ymin'])
+                    xmax = int(xscale * obj['xmax']) \
+                           + int((obj['xmax'] - input_width/2) + input_width/2)
+                    ymax = int(yscale * obj['ymax'])
+
+                    # See https://docs.opencv.org/3.4.1/d6/d6e/group__imgproc__draw.html
+                    # for more information about the cv2.rectangle method.
+                    # Method signature: image, point1, point2, color, and tickness.
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 165, 20), 10)
+                    # Amount to offset the label/probability text above the bounding box.
+                    text_offset = 15
+                    # See https://docs.opencv.org/3.4.1/d6/d6e/group__imgproc__draw.html
+                    # for more information about the cv2.putText method.
+                    # Method signature: image, text, origin, font face, font scale, color,
+                    # and tickness
+                    cv2.putText(frame, "{}: {:.2f}%".format(output_map[obj['label']],
+                                                               obj['prob'] * 100),
+                                (xmin, ymin-text_offset),
+                                cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255, 165, 20), 6)
+                    # Store label and probability to send to cloud
+                    cloud_output[output_map[obj['label']]] = obj['prob']
+
+            # Set the next frame in the local display stream.
+            local_display.set_frame_data(frame)
+            # Send results to the cloud
+            client.publish(topic=iot_topic, payload=json.dumps(cloud_output))
+    except Exception as ex:
+        client.publish(topic=iot_topic, payload='Error in object detection lambda: {}'.format(ex))
+
+greengrass_infinite_infer_run()
